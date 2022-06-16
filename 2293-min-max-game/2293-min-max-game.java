7c237458818572163d0bc7b66a538b5fb85f4bb6
VadirajKranam <90848631+VadirajKranam@users.noreply.github.com>
2022-06-16 23:25:10 +0530

    Time: 3 ms (23.99%), Space: 44 MB (44.04%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minMaxGame(int[] nums) {
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+        while(nums.length!=1){
+        int[] newNums=new int[nums.length/2];
+        for(int i=0;i<nums.length/2;i++)
+        {
+           if(i%2==0)
+           {
+               newNums[i]=Math.min(nums[2*i],nums[2*i+1]);
+           }
+            else
+            {
+                newNums[i]=Math.max(nums[2*i],nums[2*i+1]);
+            }
+        }
+        nums=newNums;
+        }
+        return nums[0];
+    }
+}
