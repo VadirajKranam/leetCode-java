55445377899d6041dff07c4bb5b5700a66fee78f
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-09-22 23:45:41 +0530

    Time: 1 ms (10.38%), Space: 42.6 MB (62.86%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -4,18 +4,19 @@ class Solution {
         {
             for(int j=i;j<matrix[i].length;j++)
             {
-                int t=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=t;
+               int t=matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=t;
             }
         }
         for(int i=0;i<matrix.length;i++)
         {
-            for(int j=0;j<matrix[i].length/2;j++)
+            int l=matrix[i].length;
+            for(int j=0;j<l/2;j++)
             {
-                int t=matrix[i][j];
-                matrix[i][j]=matrix[i][matrix[i].length-j-1];
-                matrix[i][matrix[i].length-j-1]=t;
+                int t=matrix[i][l-j-1];
+                matrix[i][l-j-1]=matrix[i][j];
+                matrix[i][j]=t;
             }
         }
     }
