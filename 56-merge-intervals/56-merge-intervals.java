61ead93ad4f293a2f8466d2db80d619d0c54e3bf
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-09-24 22:46:22 +0530

    Time: 21 ms (16.06%), Space: 54.4 MB (83.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,30 +1,47 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        ArrayList<ArrayList<Integer>> merged=new ArrayList<>();
-        Arrays.sort(intervals,(a,b)->{return a[0]-b[0];});
         for(int i=0;i<intervals.length;i++)
         {
-            for(int j=0;j<intervals[i].length;j++)
+            if(intervals[i][0]>intervals[i][1])
             {
-                if(merged.isEmpty()||merged.get(merged.size()-1).get(1)<intervals[i][0])
-                {
-                    ArrayList<Integer> t=new ArrayList<>();
-                    t.add(intervals[i][0]);
-                    t.add(intervals[i][1]);
-                    merged.add(t);
-                }
-                else
-                {
-                   merged.get(merged.size()-1).set(1,(int)Math.max(merged.get(merged.size()-1).get(1),intervals[i][1])); 
-                }
+                int t=intervals[i][0];
+                intervals[i][0]=intervals[i][1];
+                intervals[i][1]=t;
             }
         }
-        int[][] ans=new int[merged.size()][2];
+        Arrays.sort(intervals,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] i1,int[] i2)
+            {
+                return Double.compare(i1[0],i2[0]);
+            }
+        });
+     ArrayList<ArrayList<Integer>> merge=new ArrayList<>();
+        for(int i=0;i<intervals.length;i++)
+        {
+        if(merge.isEmpty()||merge.get(merge.size()-1).get(1)<intervals[i][0])
+        {
+            ArrayList<Integer> t=new ArrayList<>();
+            t.add(intervals[i][0]);
+            t.add(intervals[i][1]);
+            merge.add(t);
+        }
+      else
+      {
+          ArrayList<Integer> t=new ArrayList<>();
+          int t1=merge.get(merge.size()-1).get(0);
+ int t2=(int)(Math.max(merge.get(merge.size()-1).get(1),intervals[i][1]));
+          t.add(t1);
+          t.add(t2);
+          merge.set(merge.size()-1,t);
+      }
+        }
+        int[][] ans=new int[merge.size()][merge.get(0).size()];
         for(int i=0;i<ans.length;i++)
         {
             for(int j=0;j<ans[0].length;j++)
             {
-                ans[i][j]=merged.get(i).get(j);
+                ans[i][j]=merge.get(i).get(j);
             }
         }
         return ans;
