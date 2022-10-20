a79caa5b857414f77f99c94e60b47dc68280bb47
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-10-20 18:09:28 +0530

    Time: 32 ms (5.11%), Space: 45.8 MB (5.03%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,34 +1,37 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        ArrayList<ArrayList<Integer>> dp=new ArrayList<>();
+        int m=triangle.size();
+        int[][] dp=new int[m][];
         for(int i=0;i<triangle.size();i++)
         {
-            ArrayList<Integer> l=new ArrayList<>();
+            dp[i]=new int[triangle.get(i).size()];
+            Arrays.fill(dp[i],-9999999);
+        }
+        for(int i=0;i<dp[dp.length-1].length;i++)
+        {
+            dp[m-1][i]=triangle.get(m-1).get(i);
+        }
+        for(int i=0;i<dp.length;i++)
+        {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+        for(int i=m-2;i>=0;i--)
+        {
             for(int j=0;j<triangle.get(i).size();j++)
             {
-                l.add(-1);
+                int up=triangle.get(i).get(j)+dp[i+1][j];
+                int down=9999999;
+               
+                
+                    down=triangle.get(i).get(j)+dp[i+1][j+1];
+                
+                dp[i][j]=Math.min(up,down);
             }
-            dp.add(l);
         }
-        return function(0,0,triangle,dp);
-    }
-    public int function(int m,int n,List<List<Integer>> triangle,ArrayList<ArrayList<Integer>> dp)
-    {
-        if(m>=triangle.size() || n>=triangle.get(m).size())
+        for(int i=0;i<dp.length;i++)
         {
-            return 0;
+            System.out.println(Arrays.toString(dp[i]));
         }
-        if(m==triangle.size()-1)
-        {
-            return triangle.get(m).get(n);
-        }
-        if(dp.get(m).get(n)!=-1)
-        {
-            return dp.get(m).get(n);
-        }
-        int up=triangle.get(m).get(n)+function(m+1,n+1,triangle,dp);
-        int down=triangle.get(m).get(n)+function(m+1,n,triangle,dp);
-        dp.get(m).set(n,Math.min(up,down));
-        return dp.get(m).get(n);
+        return dp[0][0];
     }
 }
