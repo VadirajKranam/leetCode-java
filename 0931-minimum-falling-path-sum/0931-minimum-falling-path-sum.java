b5dfae334558e6eca80e49eba79baebc44b769bc
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-10-22 18:00:31 +0530

    Time: 69 ms (5.07%), Space: 42.5 MB (98.62%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,39 +1,39 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-       int[][] dp=new int[matrix.length][matrix[0].length];
-         int m=matrix.length;
+        int n=matrix.length;
+        int[][] dp=new int[n][n];
         for(int[] row:dp)
         {
             Arrays.fill(row,Integer.MAX_VALUE);
         }
-         int ans=Integer.MAX_VALUE;
-         for(int i=0;i<matrix[0].length;i++)
-         {
-             ans=Math.min(function(0,i,matrix,dp),ans);
-             for(int[] row:dp)
-             {
-               Arrays.fill(row,Integer.MAX_VALUE);
-             }
-         }
-         return ans;
-    }
-    public static int function(int m,int n,int[][] mat,int[][] dp)
-    {
-        if(m>=mat.length || n>=mat[0].length || n<0)
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++)
         {
-            return 999999;
+            ans=Math.min(function(0,i,matrix,dp),ans);
+           for(int[] row:dp)
+           {
+            Arrays.fill(row,Integer.MAX_VALUE);
+           } 
         }
-        if(m==mat.length-1)
+        return ans;
+    }
+    public int function(int m,int n,int[][] matrix,int[][] dp)
+    {
+        if(m>=matrix.length || n>=matrix[0].length || n<0)
         {
-            return mat[m][n];
+            return 9999999;
+        }
+        if(m==matrix.length-1)
+        {
+            return matrix[m][n];
         }
         if(dp[m][n]!=Integer.MAX_VALUE)
         {
             return dp[m][n];
         }
-        int down=mat[m][n]+function(m+1,n,mat,dp);
-        int dleft=mat[m][n]+function(m+1,n-1,mat,dp);
-        int dright=mat[m][n]+function(m+1,n+1,mat,dp);
-        return dp[m][n]=Math.min(down,Math.min(dleft,dright));
+        int dleft=matrix[m][n]+function(m+1,n-1,matrix,dp);
+        int d=matrix[m][n]+function(m+1,n,matrix,dp);
+        int dright=matrix[m][n]+function(m+1,n+1,matrix,dp);
+        return dp[m][n]=Math.min(d,Math.min(dleft,dright));
     }
 }
