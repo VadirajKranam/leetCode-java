9512e87813b23d2c546f47f6fffa36880514299e
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-10-26 15:05:05 +0530

    Time: 25 ms (74.83%), Space: 50.6 MB (15.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,11 +1,29 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int[][] dp=new int[s.length()+1][t.length()+1];
-        for(int[] r:dp)
+        for(int i=0;i<=t.length();i++)
         {
-            Arrays.fill(r,-1);
+            dp[0][i]=0;
         }
-        return function(s.length()-1,t.length()-1,s,t,dp);
+        for(int i=0;i<=s.length();i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i1=1;i1<dp.length;i1++)
+        {
+            for(int i2=1;i2<dp[i1].length;i2++)
+            {
+                if(s.charAt(i1-1)==t.charAt(i2-1))
+                {
+                    dp[i1][i2]=dp[i1-1][i2-1]+dp[i1-1][i2];
+                }
+                else
+                {
+                    dp[i1][i2]=dp[i1-1][i2];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
     public int function(int i,int j,String s,String t,int[][] dp)
     {
