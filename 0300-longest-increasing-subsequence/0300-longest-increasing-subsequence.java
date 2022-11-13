e758037b19caeaeed0f62d8cb4e476e72e773428
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-11-13 16:39:57 +0530

    Time: 276 ms (10.57%), Space: 242.3 MB (5.07%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,11 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length][nums.length+1];
+        int[][] dp=new int[nums.length+1][nums.length+1];
         for(int i=0;i<dp.length;i++)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
         }
-        return function(0,-1,nums,dp);
+        for(int ind=nums.length-1;ind>=0;ind--)
+        {
+            for(int pi=ind-1;pi>=-1;pi--)
+            {
+                int len=0+dp[ind+1][pi+1];
+                if(pi==-1 || nums[ind]>nums[pi])
+                {
+                    len=Math.max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][pi+1]=len;
+            }
+        }
+        return dp[0][-1+1];
     }
     public int function(int ind,int pi,int[] nums,int[][] dp)
     {
