3c7337cf8a33048a193866c8d8bca3d449bc5457
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-11-13 17:00:16 +0530

    Time: 78 ms (56.41%), Space: 43.9 MB (75.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,40 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] next=new int[nums.length+1];
-        int[] curr=new int[nums.length+1];
-        Arrays.fill(next,0);
-        Arrays.fill(curr,0);
-        
-        for(int ind=nums.length-1;ind>=0;ind--)
+        int[] dp=new int[nums.length];
+        int max=1;
+        Arrays.fill(dp,1);
+        for(int ind=0;ind<nums.length;ind++)
         {
-            for(int pi=ind-1;pi>=-1;pi--)
+            for(int prev=0;prev<ind;prev++)
             {
-                int len=0+next[pi+1];
-                if(pi==-1 || nums[ind]>nums[pi])
+                if(nums[prev]<nums[ind])
                 {
-                    len=Math.max(len,1+next[ind+1]);
+                    dp[ind]=Math.max(1+dp[prev],dp[ind]);
                 }
-                curr[pi+1]=len;
             }
-            next=curr;
+            max=Math.max(max,dp[ind]);
         }
-        return next[-1+1];
-    }
-    public int function(int ind,int pi,int[] nums,int[][] dp)
-    {
-        if(ind==nums.length)
-        {
-            return 0;
-        }
-        if(dp[ind][pi+1]!=-1)
-        {
-            return dp[ind][pi+1];
-        }
-        int len=0+function(ind+1,pi,nums,dp);
-        if(pi==-1 || nums[ind]>nums[pi])
-        {
-            len=Math.max(len,1+function(ind+1,ind,nums,dp));
-        }
-        return dp[ind][pi+1]=len;
+        return max;
     }
 }
