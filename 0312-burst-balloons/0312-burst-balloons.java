44bd6dd5e0938addf021f71afe6eda91e9518906
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-11-16 18:03:16 +0530

    Time: 72 ms (77.28%), Space: 42.8 MB (69.39%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -10,9 +10,26 @@ class Solution {
         int[][] dp=new int[bals.length+1][bals.length+1];
         for(int[] d:dp)
         {
-            Arrays.fill(d,-1);
+            Arrays.fill(d,0);
         }
-        return function(1,nums.length,bals,dp);
+        for(int i=nums.length;i>=1;i--)
+        {
+            for(int j=1;j<=nums.length;j++)
+            {
+                if(i>j)
+                {
+                    continue;
+                }
+                int max=Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost=bals[i-1]*bals[k]*bals[j+1]+dp[i][k-1]+dp[k+1][j];
+                    max=Math.max(max,cost);
+                }
+                dp[i][j]=max;
+            }
+        }
+        return dp[1][nums.length];
     }
     public int function(int i,int j,int[] nums,int[][] dp)
     {
