d0d2eba56c34b6f6d66970ed3d225b3e92de97ea
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-11-17 17:32:03 +0530

    Time: 6 ms (95.08%), Space: 42 MB (81.65%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -18,26 +18,27 @@ class Solution {
         }
         return dp[0];
     }
-    public int function(int i,int[] arr,int k,int[] dp)
-    {
-        if(i==arr.length)
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int maxAns=Integer.MIN_VALUE;
-        int maxi=Integer.MIN_VALUE;
-        int len=0;
-        for(int j=i;j<Math.min(arr.length,i+k);j++)
-        {
-            len++;
-            maxi=Math.max(maxi,arr[j]);
-            int sum=len*maxi+function(j+1,arr,k,dp);
-            maxAns=Math.max(maxAns,sum);
-        }
-        return dp[i]=maxAns%1000000007;
-    }
+    // memoization code
+    // public int function(int i,int[] arr,int k,int[] dp)
+    // {
+    //     if(i==arr.length)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i]!=-1)
+    //     {
+    //         return dp[i];
+    //     }
+    //     int maxAns=Integer.MIN_VALUE;
+    //     int maxi=Integer.MIN_VALUE;
+    //     int len=0;
+    //     for(int j=i;j<Math.min(arr.length,i+k);j++)
+    //     {
+    //         len++;
+    //         maxi=Math.max(maxi,arr[j]);
+    //         int sum=len*maxi+function(j+1,arr,k,dp);
+    //         maxAns=Math.max(maxAns,sum);
+    //     }
+    //     return dp[i]=maxAns%1000000007;
+    // }
 }
