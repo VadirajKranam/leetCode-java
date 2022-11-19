f6afd402bafe874cac91dc05e2e00d9b2dd3c16c
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-11-19 21:09:43 +0530

    Time: 296 ms (8.87%), Space: 50.5 MB (7.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -18,12 +18,12 @@ class Solution {
         {
             return dp[ind];
         }
-        int j=1;
-        int ans=9999999;
         if(nums[ind]==0)
         {
             return 99999999;
         }
+        int j=1;
+        int ans=9999999;
         while(j<=nums[ind])
         {
             ans=Math.min(ans,1+function(nums,ind+j,dp));
