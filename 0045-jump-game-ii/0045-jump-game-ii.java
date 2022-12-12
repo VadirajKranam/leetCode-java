1679d6e56e8f374b8183354f5b449bfaff73bb7e
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-12-13 00:04:47 +0530

    Time: 291 ms (9.14%), Space: 50.1 MB (14.66%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,34 +1,28 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp=new int[nums.length];
+         int[] dp=new int[nums.length+1];
         Arrays.fill(dp,-1);
-        return function(nums,0,dp);
+        return function(0,nums,dp);
     }
-    public int function(int[] nums,int ind,int[] dp)
+    public int function(int ind,int[] nums,int[] dp)
     {
-        if(ind>=nums.length)
-        {
-            return 9999999;
-        }
         if(ind==nums.length-1)
         {
             return 0;
+        }
+        if(ind>=nums.length)
+        {
+            return 99999999;
         }
         if(dp[ind]!=-1)
         {
             return dp[ind];
         }
-        if(nums[ind]==0)
+        int min=99999999;
+        for(int i=1;i<=nums[ind];i++)
         {
-            return 99999999;
+             min=Math.min(min,1+function(ind+i,nums,dp));
         }
-        int j=1;
-        int ans=9999999;
-        while(j<=nums[ind])
-        {
-            ans=Math.min(ans,1+function(nums,ind+j,dp));
-            j++;
-        }
-      return  dp[ind]=ans;
+        return dp[ind]=min;
     }
 }
