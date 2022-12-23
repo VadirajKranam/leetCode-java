a1042f7bc46d97b59839d2ab4fdb4ec0ca0ff982
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-12-23 11:00:07 +0530

    Time: 3 ms (30.66%), Space: 42.7 MB (24.06%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,47 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp=new int[prices.length+2][2];
-        for(int i=0;i<dp.length;i++)
-        {
-            Arrays.fill(dp[i],0);
-        }
-        for(int ind=prices.length-1;ind>=0;ind--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                int profit=0;
-                if(buy==1)
-                {
-                  profit=Math.max((-prices[ind]+dp[ind+1][0]),dp[ind+1][1]);
-                }
-                else
-                {
-                    profit=Math.max((prices[ind]+dp[ind+2][1]),dp[ind+1][0]);
-                }
-                dp[ind][buy]=profit;
-            }
-        }
-        return dp[0][1];
+        int[][] dp=new int[prices.length+1][2];
+        for(int[] d:dp)
+        {Arrays.fill(d,-1);}
+        return function(0,prices,dp,1);
     }
-    int function(int ind,int[] prices,int buy,int[][] dp)
+    public int function(int ind,int[] prices,int[][] dp,int canBuy)
     {
         if(ind>=prices.length)
         {
             return 0;
         }
-        if(dp[ind][buy]!=-1)
+        if(dp[ind][canBuy]!=-1)
         {
-            return dp[ind][buy];
+            return dp[ind][canBuy];
         }
         int profit=0;
-        if(buy==1)
+        if(canBuy==1)
         {
-            profit=Math.max((-prices[ind]+function(ind+1,prices,0,dp)),function(ind+1,prices,1,dp));
+            profit=Math.max((-prices[ind]+function(ind+1,prices,dp,0)),function(ind+1,prices,dp,canBuy));
         }
         else
         {
-            profit=Math.max((prices[ind]+function(ind+2,prices,1,dp)),function(ind+1,prices,0,dp));
+            profit=Math.max((prices[ind]+function(ind+2,prices,dp,1)),function(ind+1,prices,dp,0));
         }
-        return dp[ind][buy]=profit;
+        return dp[ind][canBuy]=profit;
     }
 }
