465badef26f57ce4f908ce97b841be4509e4d5c2
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2022-12-26 06:53:15 +0530

    Time: 426 ms (13.96%), Space: 43.2 MB (75.50%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,34 +1,38 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-       int[] dp=new int[nums.length+1];
+        if(nums.length==1)
+        {
+            return true;
+        }
+        int[] dp=new int[nums.length+1];
         Arrays.fill(dp,-1);
         return function(0,nums,dp);
     }
     public boolean function(int ind,int[] nums,int[] dp)
     {
-        if(ind==nums.length-1)
-        {
-            return true;
-        }
         if(ind>=nums.length)
         {
             return false;
+        }
+        if(ind==nums.length-1)
+        {
+            dp[ind]=1;
+            return true;
         }
         if(dp[ind]!=-1)
         {
             return dp[ind]==1;
         }
+        int check=0;
         for(int i=1;i<=nums[ind];i++)
         {
             if(function(ind+i,nums,dp))
             {
-                dp[ind]=1;
-                return true;
+                check=1;
+                break;
             }
         }
-        
-        dp[ind]=0;
-        return false;
+        dp[ind]=check;
+        return dp[ind]==1;
     }
 }
