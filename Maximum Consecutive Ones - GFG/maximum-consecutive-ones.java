b77765f312b4c9925a61119d23c29f46b70623a8
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-04-16 14:45:30 +0530

    Added solution - LeetHub

---
+++ Maximum Consecutive Ones - GFG/maximum-consecutive-ones.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.longestOnes(n, nums, k);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int longestOnes(int n, int[] nums, int k) {
+        int zero=0,left=0,ans=0;
+        for(int right=0;right<nums.length;right++)
+        {
+            if(nums[right]==0)
+            {
+                zero++;
+            }
+            if(zero>k)
+            {
+                if(nums[left]==0)
+                {
+                    zero--;
+                }
+                left++;
+            }
+            if(zero<=k)
+            {
+                ans=Math.max(ans,right-left+1);
+            }
+        }
+        return ans;
+    }
+}
+        
