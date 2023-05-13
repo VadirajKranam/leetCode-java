b296ec7e20c5cce6dab1b46df752f1600889f8c0
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-05-13 16:36:19 +0530

    Added solution - LeetHub

---
+++ Bit Magic - GFG/bit-magic.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.bitMagic(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int bitMagic(int n, int[] arr) {
+        int i=0,j=arr.length-1;
+        int count=0;
+        while(i<j)
+        {
+            if(arr[i]!=arr[j])
+            {
+                int n1=arr[i];
+                int n2=arr[j];
+                arr[i]=n1^n2;
+                arr[j]=n1^n2;
+                count++;
+            }
+            i++;
+            j--;
+        }
+        return (count+1)/2;
+    }
+}
+        
