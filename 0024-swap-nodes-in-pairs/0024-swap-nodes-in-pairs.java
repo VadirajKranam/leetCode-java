55d050580b8e74feea523c189f8367f1c98ddadb
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-05-16 22:07:42 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (80.70%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -17,26 +17,24 @@ class Solution {
             a.add(t);
             t=t.next;
         }
+         if(a.size()==1 || a.size()==0)
+        {
+            return head;
+        }
         for(int i=0;i<a.size()-1;i+=2)
         {
             ListNode t1=a.get(i);
             a.set(i,a.get(i+1));
             a.set(i+1,t1);
         }
-        if(a.size()==1 || a.size()==0)
-        {
-            return head;
-        }
         a.get(a.size()-1).next=null;
         head=a.get(0);
         t=head;
         for(int i=1;i<a.size();i++)
         {
-            //System.out.println(t.val);
             t.next=a.get(i);
             t=t.next;
         }
-        //System.out.println(t.val);
         return head;
     }
 }
