ec38dd213a1bdef40a1dd31182712ea00b02f6c3
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-05-24 12:38:52 +0530

    Added solution - LeetHub

---
+++ Majority Vote - GFG/majority-vote.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.Solve(n, nums);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> Solve(int n, int[] nums) {
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(Map.Entry<Integer,Integer> e:m.entrySet())
+        {
+            if(e.getValue()>n/3)
+            {
+                ans.add(e.getKey());
+            }
+        }
+        if(ans.size()==0)
+        {
+            ans.add(-1);
+        }
+        return ans;
+    }
+}
+        
