2351b0edb7a10ec78551fbefebcf053b5bd3f324
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-06-12 11:41:01 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -4,17 +4,21 @@ import java.util.*;
 
 class RodCutting {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
         while (t-- > 0) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
 
             Solution ob = new Solution();
-            System.out.println(ob.cutRod(arr, n));
+            out.println(ob.cutRod(arr, n));
         }
+        out.close();
     }
 }
 
@@ -23,12 +27,12 @@ class RodCutting {
 
 class Solution{
     public int cutRod(int price[], int n) {
-        int[][] dp=new int[price.length+1][n+1];
-        for(int[] d:dp)
-        {
-            Arrays.fill(d,-1);
-        }
-        return function(price.length-1,price,n,dp);
+       int[][] dp=new int[price.length+1][n+1];
+       for(int[] d:dp)
+       {
+           Arrays.fill(d,-1);
+       }
+       return function(price.length-1,price,n,dp);
     }
     public int function(int i,int[] price,int n,int[][] dp)
     {
