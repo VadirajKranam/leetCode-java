a0295c4d298fdb28d3090ef1d1460d456e5c27f2
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-06-20 13:07:44 +0530

    Added solution - LeetHub

---
+++ Matchsticks game - GFG/matchsticks-game.java
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.matchGame(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int matchGame(Long N) {
+        return N%5l==0l?-1:(int)(N%5l);
+    }
+};
