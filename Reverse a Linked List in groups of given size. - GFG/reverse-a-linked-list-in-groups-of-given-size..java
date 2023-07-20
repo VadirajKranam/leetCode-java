82691c1e7064c6b2b76f2f331aaa5bef9a1f513b
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-07-21 00:18:42 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..java
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res,out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node node,PrintWriter out)
+    {
+        while(node != null)
+        {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        ArrayList<Integer> a1=new ArrayList<>();
+         ArrayList<Integer> a2=new ArrayList<>();
+        int t=k;
+        Node n1=node;
+        while(n1!=null)
+        {
+            if(t==0)
+            {
+                Collections.reverse(a1);
+                a2.addAll(a1);
+                a1.clear();
+                t=k;
+            }
+            a1.add(n1.data);
+            n1=n1.next;
+            t--;
+        }
+         Collections.reverse(a1);
+         a2.addAll(a1);
+         n1=node;
+         for(int i=0;i<a2.size();i++)
+         {
+             n1.data=a2.get(i);
+             n1=n1.next;
+         }
+        return node;
+    }
+}
+
