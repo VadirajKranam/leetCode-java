9790e2ec05eb096dc3e5231c52887391a62dd6ce
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-07-25 00:08:45 +0530

    Added solution - LeetHub

---
+++ Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.java
@@ -0,0 +1,195 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+ 
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+ 
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                Spiral g = new Spiral();
+                ArrayList<Integer> result = g.findSpiral(root);
+                for(int value : result)
+                System.out.print(value + " ");
+			    System.out.println();
+    	        
+	        }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Spiral
+{
+    //Function to return a list containing the level order 
+    //traversal in spiral form.	
+    ArrayList<Integer> findSpiral(Node root) 
+    {
+        ArrayList<ArrayList<Integer>> temp=new ArrayList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<Node> q=new LinkedList<>();
+        Node node=root;
+        if(node==null)
+        {
+            return ans;
+        }
+        q.offer(node);
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            
+            ArrayList<Integer> in=new ArrayList<>();
+            for(int i=0;i<size;i++)
+            {
+                Node n=q.poll();
+                in.add(n.data);
+                if(n.left!=null)
+                {
+                    q.offer(n.left);
+                }
+                if(n.right!=null)
+                {
+                    q.offer(n.right);
+                }
+            }
+            temp.add(in);
+        }
+        int check=1;
+        ans.add(temp.get(0).get(0));
+        for(int i=1;i<temp.size();i++)
+        {
+            if(check==1)
+            {
+                for(int j=0;j<temp.get(i).size();j++)
+                {
+                    ans.add(temp.get(i).get(j));
+                }
+                check=-1;
+            }
+            else
+            {
+                for(int j=temp.get(i).size()-1;j>=0;j--)
+                {
+                    ans.add(temp.get(i).get(j));
+                }
+                check=1;
+            }
+        }
+        return ans;
+    }
+}
