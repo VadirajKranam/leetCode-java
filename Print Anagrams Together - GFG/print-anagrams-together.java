813cd046ab72ce1dde9c3c0ffcc2fcca098d07f0
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-08-04 00:23:05 +0530

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine().trim());
+	    while(t > 0)
+	    {
+	        int n= Integer.parseInt(br.readLine().trim());
+	        String x = br.readLine().trim();
+	        String string_list[] = x.split(" ",n);
+	        
+	        Solution ob = new  Solution();
+	        
+	        List <List<String>> ans = ob.Anagrams(string_list);
+	        
+	        Collections.sort(ans, new Comparator<List<String>>(){
+            public int compare(List<String> l1, List<String> l2) {
+                    String s1 =  l1.get(0);
+                    String s2 = l2.get(0);
+                    
+                    return s1.compareTo(s2);
+                }
+            });
+	        
+	        for(int i=0;i<ans.size();i++)
+	        {
+	            for(int j=0;j<ans.get(i).size();j++)
+	            {
+	                System.out.print(ans.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	       
+	       
+            t--;
+	    }
+	}
+    
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public List<List<String>> Anagrams(String[] string_list) {
+      List<List<String>> ans=new ArrayList<>();
+      Map<String,ArrayList<String>> m=new HashMap<>();
+      for(int i=0;i<string_list.length;i++)
+      {
+          char[] t1=string_list[i].toCharArray();
+          Arrays.sort(t1);
+          String t2=new String(t1);
+          if(!m.containsKey(t2))
+          {
+              m.put(t2,new ArrayList<>());
+          }
+          m.get(t2).add(string_list[i]);
+      }
+      for(Map.Entry<String,ArrayList<String>> e:m.entrySet())
+      {
+          ans.add(e.getValue());
+      }
+      return ans;
+    }
+}
