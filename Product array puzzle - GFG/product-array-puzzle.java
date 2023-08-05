4aa9285a58a970451c5d026a7905d1ce27c9fabb
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-08-05 23:24:13 +0530

    Added solution - LeetHub

---
+++ Product array puzzle - GFG/product-array-puzzle.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+        	int[] array = new int[n];
+        	for (int i=0; i<n ; i++ ) {
+        		array[i] = sc.nextInt();
+        	}
+            Solution ob = new Solution();
+            long[] ans = new long[n];
+            ans = ob.productExceptSelf(array, n); 
+
+           	for (int i = 0; i < n; i++) { 
+				System.out.print(ans[i]+" ");
+			} 
+            System.out.println();
+            t--;
+        }
+    } 
+} 
+  
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution 
+{ 
+	public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+       int count=0;
+       for(int i=0;i<nums.length;i++)
+       {
+           if(nums[i]==0)
+           {
+               count++;
+           }
+       }
+       if(count>1)
+       {
+          long[] ans=new long[nums.length];
+          Arrays.fill(ans,0);
+          return ans;
+       }
+       long pro=1;
+       for(int i=0;i<nums.length;i++)
+       {
+           if(nums[i]!=0)
+           {
+               pro*=nums[i];
+           }
+       }
+       long[] ans=new long[nums.length];
+       for(int i=0;i<nums.length;i++)
+       {
+           if(nums[i]!=0 && count==1)
+           {
+               ans[i]=0;
+           }
+           else if(nums[i]==0 && count==1)
+           {
+               ans[i]=pro;
+           }
+           else
+           {
+               ans[i]=pro/nums[i];
+           }
+       }
+       return ans;
+	} 
+} 
