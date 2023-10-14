3fd63f62f965df7d0a79ac7325de4237d0e1ad9f
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2023-10-14 10:52:14 +0530

    Added solution - LeetHub

---
+++ Find Common Nodes in two BSTs - GFG/find-common-nodes-in-two-bsts.java
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to find the nodes that are common in both BST.
+	public static ArrayList<Integer> findCommon(Node root1,Node root2)
+    {
+        Set<Integer> s=new HashSet<>();
+        fun1(root1,s);
+        ArrayList<Integer> ans=new ArrayList<>();
+        fun2(root2,s,ans);
+        return ans;
+    }
+    public static void fun1(Node n1,Set<Integer> s)
+    {
+        if(n1==null)
+        {
+            return;
+        }
+        fun1(n1.left,s);
+        s.add(n1.data);
+        fun1(n1.right,s);
+    }
+    public static void fun2(Node n2,Set<Integer> s,ArrayList<Integer> arr)
+    {
+        if(n2==null)
+        {
+            return;
+        }
+        fun2(n2.left,s,arr);
+        if(s.contains(n2.data))
+        {
+            arr.add(n2.data);
+        }
+        fun2(n2.right,s,arr);
+    }
+}
+
+
+//{ Driver Code Starts.
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+            
+            s = br.readLine();
+            Node root2 = buildTree(s);
+            
+            Solution g = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = g.findCommon(root1,root2);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+            
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
