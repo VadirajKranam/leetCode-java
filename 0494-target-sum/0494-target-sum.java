67797d35c63faf78c9c9f7c4577a168a3f203a14
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2024-02-12 10:44:00 +0530

    Time: 329 ms (36.04%), Space: 41.1 MB (84.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,22 +2,22 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         return function(0,nums,target);
     }
-    public int function(int i,int[] nums,int sum)
+    public int function(int i,int[] nums,int target)
     {
         if(i==nums.length-1)
         {
-            if(nums[i]+sum==0 && nums[i]-sum==0)
+            if(target+nums[i]==0 && target-nums[i]==0)
             {
                 return 2;
             }
-            if(nums[i]+sum==0 || nums[i]-sum==0)
+            if(target-nums[i]==0 || target+nums[i]==0)
             {
                 return 1;
             }
-             return 0;
+            return 0;
         }
-        int plus=function(i+1,nums,sum+nums[i]);
-        int minus=function(i+1,nums,sum-nums[i]);
+        int plus=function(i+1,nums,target+nums[i]);
+        int minus=function(i+1,nums,target-nums[i]);
         return plus+minus;
     }
 }
