35b59dc4e52291c368c7d227dfba3045a6cd12d8
Vadiraj Karanam <90848631+VadirajKranam@users.noreply.github.com>
2024-02-12 11:02:05 +0530

    Time: 14 ms (21.52%), Space: 47.9 MB (26.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -9,24 +9,24 @@ class Solution {
     }
     public int function(int i,int[] coins,int amount,int[][] dp)
     {
-        if(i==coins.length)
+        if(amount<0 || i==coins.length)
         {
-            if(amount==0)
-            {
-                return 1;
-            }
             return 0;
+        }
+        if(amount==0)
+        {
+            return 1;
         }
         if(dp[i][amount]!=-1)
         {
             return dp[i][amount];
         }
-        int notPick=function(i+1,coins,amount,dp);
-        int pick=0;
+        int take=0;
         if(coins[i]<=amount)
         {
-            pick=function(i,coins,amount-coins[i],dp);
+            take=function(i,coins,amount-coins[i],dp);
         }
-        return dp[i][amount]=pick+notPick;
+        int notTake=function(i+1,coins,amount,dp);
+        return dp[i][amount]=take+notTake;
     }
 }
